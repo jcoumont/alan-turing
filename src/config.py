d81e77c011c1677a10bbb78947a15dc868a1c849
jcoumont <jcoumont@gmail.com>
2020-11-28 21:56:34 +0100

    merge with DEV

--- src/config.py
+++ src/config.py
@@ -1,31 +1,34 @@
-
 from src import Database
 
 from discord.ext.commands import Bot
+
 # from os import environ
 
 
 class Config:
-
     def __init__(self):
 
         # ENV variables
         # self.DISCORD_TOKEN = environ.get('DISCORD_TOKEN')
         # self.DISCORD_CHANNEL_ID = int(environ.get('DISCORD_CHANNEL'))
         # self.WEBHOOK = environ.get('WEBHOOK')
-        self.DISCORD_TOKEN = 'NzczMTUxNjE2MTEwODIxNDE2.X6FDjQ.iwIFli_40zZWCYrnlTuqnOmykE4'
+        self.DISCORD_TOKEN = (
+            "NzczMTUxNjE2MTEwODIxNDE2.X6FDjQ.Gbh2mlw7pgh_7UlM5Xn2-8jTJ8Q"
+        )
         self.DISCORD_CHANNEL_ID = 773154253496909827
-        self.WEBHOOK = 'https://discord.com/api/webhooks/773160572308160523/9zmjE75bsOWMGQcKGb2_5P0JBjkuQLfSgrPNx55WxDMkOOAZ_7rhSzTi9qE_b3X-YiK4'
+        self.WEBHOOK = "https://discord.com/api/webhooks/773160572308160523/9zmjE75bsOWMGQcKGb2_5P0JBjkuQLfSgrPNx55WxDMkOOAZ_7rhSzTi9qE_b3X-YiK4"
 
         # Discord client
-        self.discord = Bot(command_prefix='!')
+        self.discord = Bot(command_prefix="!")
 
         # Database
         self.db = Database()
 
         # Attendance globals
         self.last_message = None
-        self.last_attendance = None  # Attendance (Period, at_home), see >> AttendanceMessage.py
+        self.last_attendance = (
+            None  # Attendance (Period, at_home), see >> AttendanceMessage.py
+        )
 
 
 config = Config()
